5375a6c44a4bbca8bcb20b5010dabe40bf828174
c_reg_c_lark <lumberjackchester@gmail.com>
2018-05-10 14:00:37 -0400

    show errors

--- news.php
+++ news.php
@@ -1,4 +1,9 @@
 <?php
+error_reporting(0);
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+error_reporting(E_ALL & ~E_NOTICE);
+error_reporting(E_ALL);
 
 require_once __DIR__ . '/library/vendor/autoload.php'; // change path as needed
 
