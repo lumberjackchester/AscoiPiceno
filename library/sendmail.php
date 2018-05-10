9ecaafa19286366fb4e08cca037ba64ebd1f5755
c_reg_c_lark <lumberjackchester@gmail.com>
2018-05-10 13:51:40 -0400

    updates to site

--- library/sendmail.php
+++ library/sendmail.php
@@ -1,77 +1,76 @@
 <?php
-  /**
-   * Sets error header and json error message response.
-   *
-   * @param  String $messsage error message of response
-   * @return void
-   */
-  function errorResponse ($messsage) {
+
+/**
+ * Sets error header and json error message response.
+ *
+ * @param  String $messsage error message of response
+ * @return void
+ */
+function errorResponse($messsage) {
     header('HTTP/1.1 500 Internal Server Error');
     die(json_encode(array('message' => $messsage)));
-  }
+}
 
-  /**
-   * Pulls posted values for all fields in $fields_req array.
-   * If a required field does not have a value, an error response is given.
-   */
-  function constructMessageBody () {
-    $fields_req =  array("name" => true, "email" => true, "message" => true);
+/**
+ * Pulls posted values for all fields in $fields_req array.
+ * If a required field does not have a value, an error response is given.
+ */
+function constructMessageBody() {
+    $fields_req = array("name" => true, "email" => true, "message" => true);
     $message_body = "";
     foreach ($fields_req as $name => $required) {
-      $postedValue = $_POST[$name];
-      if ($required && empty($postedValue)) {
-        errorResponse("$name is required.");
-      } else {
-        $message_body .= ucfirst($name) . ":  " . $postedValue . "\n";
-      }
+        $postedValue = $_POST[$name];
+        if ($required && empty($postedValue)) {
+            errorResponse("$name is required.");
+        } else {
+            $message_body .= ucfirst($name) . ":  " . $postedValue . "\n";
+        }
     }
     return $message_body;
-  }
+}
 
-  header('Content-type: application/json');
-  
-  
-    $access = date("Y/m/d H:i:s");
-    error_log("Sending Captcha: $access value: ({$_SERVER['RECAPTCHA_SECRET_KEY']})");
-  
+header('Content-type: application/json');
 
-  //do Captcha check, make sure the submitter is not a robot:)...
-  $url = 'https://www.google.com/recaptcha/api/siteverify';
- $opts = array('http' =>
+
+$access = date("Y/m/d H:i:s");
+error_log("Sending Captcha: $access value: ({$_SERVER['RECAPTCHA_SECRET_KEY']})");
+
+
+//do Captcha check, make sure the submitter is not a robot:)...
+$url = 'https://www.google.com/recaptcha/api/siteverify';
+$opts = array('http' =>
     array(
-      'method'  => 'POST',
-      'header'  => 'Content-type: application/x-www-form-urlencoded',
-      'content' => http_build_query(array('secret' => $_SERVER['RECAPTCHA_SECRET_KEY'], 'response' => $_POST["g-recaptcha-response"]))
+        'method' => 'POST',
+        'header' => 'Content-type: application/x-www-form-urlencoded',
+        'content' => http_build_query(array('secret' => $_SERVER['RECAPTCHA_SECRET_KEY'], 'response' => $_POST["g-recaptcha-response"]))
     )
-  );
-  
-    $access = date("Y/m/d H:i:s");
-    
-    error_log("Sending Captcha: $access {$opts} ({$_SERVER['HTTP_USER_AGENT']})");
-  $context  = stream_context_create($opts);
-  $result = json_decode(file_get_contents($url, false, $context, -1, 40000));
+);
 
-  if (!$result->success) {
+$access = date("Y/m/d H:i:s");
+
+error_log("Sending Captcha: $access {$opts} ({$_SERVER['HTTP_USER_AGENT']})");
+$context = stream_context_create($opts);
+$result = json_decode(file_get_contents($url, false, $context, -1, 40000));
+
+if (!$result->success) {
 
     $access = date("Y/m/d H:i:s");
-  syslog(LOG_WARNING, "Captcha Failure: $access {$opts} ({$_SERVER['HTTP_USER_AGENT']})");
+    syslog(LOG_WARNING, "Captcha Failure: $access {$opts} ({$_SERVER['HTTP_USER_AGENT']})");
     errorResponse('reCAPTCHA checked failed!');
-  }
-  
- ////attempt to send email
-  $headers = 'From: admin@picenowines.com' . "\r\n" .
-    'Reply-To: lumberjackchester@gmail.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+}
+
+////attempt to send email
+$headers = 'From: admin@picenowines.com' . "\r\n" .
+        'Reply-To: polidorijack@gmail.com' . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
 $messageBody = constructMessageBody();
-syslog (LOG_DEBUG, $messageBody);
-$mailed = mail($_SERVER['FEEDBACK_EMAIL'] , "Someone submitted thier contact info on PicenoWines.com.", $messageBody, $headers);
-if($mailed){    
+syslog(LOG_DEBUG, $messageBody);
+$mailed = mail($_SERVER['FEEDBACK_EMAIL'], "Someone submitted thier contact info on PicenoWines.com.", $messageBody, $headers);
+if ($mailed) {
     http_response_code(200);
-   $mess =  json_encode("Your contact information has been sent to Jack!");
+    $mess = json_encode("Your contact information has been sent to Jack!");
     echo $mess;
-
-}else{
+} else {
     echo "Email was not sent :(";
-} 
-
+}
 ?>
---
+++ news.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once __DIR__ . '/library/vendor/autoload.php'; // change path as needed
+
+$fb = new \Facebook\Facebook([
+  'app_id' => '803185256469448',
+  'app_secret' => 'aa09c38b69f0bd9bb35b383d5ff16f79',
+  'default_graph_version' => 'v2.10',
+  'default_access_token' => '5106fcd73161d7db63f69fc7bb52e10b', // optional
+]);
+
+// Use one of the helper classes to get a Facebook\Authentication\AccessToken entity.
+//   $helper = $fb->getRedirectLoginHelper();
+//   $helper = $fb->getJavaScriptHelper();
+//   $helper = $fb->getCanvasHelper();
+//   $helper = $fb->getPageTabHelper();
+
+try {
+  // Get the \Facebook\GraphNodes\GraphUser object for the current user.
+  // If you provided a 'default_access_token', the '{access-token}' is optional.
+  $response = $fb->get('/me');
+} catch(\Facebook\Exceptions\FacebookResponseException $e) {
+  // When Graph returns an error
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(\Facebook\Exceptions\FacebookSDKException $e) {
+  // When validation fails or other local issues
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+
+$me = $response->getGraphUser();
+echo 'Logged in as ' . $me->getName();
