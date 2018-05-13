68aea9ae25fd9f332b13c858902f08cc4f0bc8ce
c_reg_c_lark <lumberjackchester@gmail.com>
2018-05-13 09:22:53 -0400

    adding fb feed

--- news.php
+++ news.php
@@ -62,21 +62,38 @@ ini_set('date.timezone', 'America/New_York');
 
 $appID = '803185256469448';
 $appSecret = 'aa09c38b69f0bd9bb35b383d5ff16f79';
- 
+
 //Create an access token using the APP ID and APP Secret.
 $accessToken = $appID . '|' . $appSecret;
- 
+
 //The ID of the Facebook page in question.
 $id = 'PicenoWines';
- 
-//Tie it all together to construct the URL
-$url = "https://graph.facebook.com/$id/posts?access_token=$accessToken";
- 
-//Make the API call
-$result = file_get_contents($url);
- 
-//Decode the JSON result.
-$decoded = json_decode($result, true);
- 
-//Dump it out onto the page so that we can take a look at the structure of the data.
-var_dump($decoded);
+
+// 
+////Tie it all together to construct the URL
+//$url = "https://graph.facebook.com/$id/posts?access_token=$accessToken";
+// 
+////Make the API call
+//$result = file_get_contents($url);
+// 
+////Decode the JSON result.
+//$decoded = json_decode($result, true);
+// 
+////Dump it out onto the page so that we can take a look at the structure of the data.
+//var_dump($decoded);
+
+try {
+    // Returns a `Facebook\FacebookResponse` object
+    $response = $fb->get(
+            '/' . $id . '/feed', $accessToken
+    );
+} catch (Facebook\Exceptions\FacebookResponseException $e) {
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+} catch (Facebook\Exceptions\FacebookSDKException $e) {
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+}
+$graphNode = $response->getGraphNode();
+
+var_dump($graphNode);
